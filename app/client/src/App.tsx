16508b88501b0e5ed203fb8b3777a3575dd49e6e
Jonathan <jonathan.huertas.contreras@gmail.com>
2025-03-03 19:21:17 -0500

    feat: :art: update the index and app
    improvement style and order

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,8 +1,12 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Lumie</h1>
-      <h2>Projects & Tasks</h2>
+      <div className="min-h-screen bg-gray-100">
+        <main className="container mx-auto px-4 py-8">
+          <h1 className="text-3xl font-bold">Lumie</h1>
+          <h2>Projects & Tasks</h2>
+        </main>
+      </div>
     </>
   );
 }
